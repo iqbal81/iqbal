e90225e2ac88f66c2b2e88a5de333de893fc9a43
iqbal81 <58505623+iqbal81@users.noreply.github.com>
2019-12-06 13:44:38 +0530

    Add files via upload

---
+++ homepage.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+<title>Home Page</title>
+<link rel="stylesheet" href="style.css" type="text/css">
+<style type="text/css">
+body
+{
+	background-image: url('back.jpg');
+	background-repeat: no-repeat;
+	background-size: 100%,100%;
+}
+</style>
+</head>
+<body>
+	<div id="main-wrapper">
+		<center><h2>Home Page</h2></center>
+		<center><h3>Welcome </h3></center>
+		
+		<form action="index.php" method="post">
+		<center><img src="logo.jpg" height="400" width="400"></center>
+		<br>
+		<button class="logout_button"><a href="login.php">
+		LOG IN</a></button>
+			</div>
+		</form>
+	</div>
+</body>
+</html>
