ff869acd2c2ab8811e69e34fbe7148a2c7e96d06
iqbal81 <58505623+iqbal81@users.noreply.github.com>
2019-12-31 14:59:52 +0530

    Add files via upload

---
+++ java/apllet mouse events.java
@@ -0,0 +1,39 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.applet.*;
+/*<applet code="three.class" width=100 height=100>
+</applet>
+*/
+public class three extends Applet
+{
+ JPanel p;
+ JTextField t;
+ String message;
+ public void init()
+ {
+ t=new JTextField(20);
+ add(t);
+  addMouseListener(new MouseAdapter()
+  {
+   public void mouseClicked(MouseEvent me)
+   {
+   message ="Mouse Clicked:X="+me.getX()+"Y="+me.getY();
+   t.setText(message);
+   }
+  });
+  addMouseMotionListener(new MouseMotionAdapter()
+  {
+   public void mouseMoved(MouseEvent me)
+   {
+   message ="Mouse Moved:X="+me.getX()+"Y="+me.getY();
+   t.setText(message);
+   }
+  });
+}
+public static void main(String args[])
+{
+	new three();
+}
+  }
+ 
