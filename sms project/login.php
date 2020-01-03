d63976befbb834ffe6e8217c44f540db9b38f3c6
iqbal81 <58505623+iqbal81@users.noreply.github.com>
2020-01-03 14:04:12 +0530

    Add files via upload

---
+++ sms project/login.php
@@ -0,0 +1,82 @@
+<html>
+<head>
+<title>Login Page</title>
+<link rel="stylesheet" href="style.css">
+<style type="text/css">
+body{
+	background-image: url('log.jpg');
+	background-repeat: no-repeat;
+	background-size: 100%,100%;
+}
+
+#mw{
+	height: 650px;
+	border-radius : 10px;
+	background-color: #e74c3c;
+	border: 3px solid #f1f1f1;
+	width: 500px;
+	margin: 0 auto;
+}
+</style>
+</head>
+<body>
+	<div id="mw">
+	
+			<div class="imgcontainer">
+			<img src="login.jpg" height="300" width="400">
+			</div>
+		<form action="<?php $_SERVER['PHP_SELF']; ?>" method="post">
+		
+			<div class="inner_container">
+				<label><b>Username</b></label>
+				<input type="text" placeholder="Enter Username" name="username" required>
+				<label><b>Password</b></label>
+				<input type="password" placeholder="Enter Password" name="password" required>
+				<button class="login_button" name="login" type="submit">Login</button>
+				<a href="register.php"><button type="button" class="register_btn">Register</button></a>
+				<a href="forget.php">Forget Password</a>
+			</div>
+		</form>
+		
+<?php
+if($_SERVER["REQUEST_METHOD"]=="POST")
+{
+  $email=$_POST["username"];
+  $pwd=$_POST["password"];
+  $dbname="sms";
+  $server="localhost";
+  $user="root";
+  $password="";
+  $cnt=0;
+  $conn=new mysqli($server,$user,$password);
+  mysqli_select_db($conn,$dbname);
+
+  $value="SELECT * FROM student";
+  $result=$conn->query($value);
+  if($result->num_rows > 0)
+  {
+
+    while ($row = $result->fetch_assoc())
+    {
+      if($row["name"]==$email && $pwd==$row["password"])
+        $cnt=0;
+      else
+      {
+        $cnt=1;
+      }
+    }
+  }
+  if($cnt==1)
+  {
+    $message = "Invalid Email and Password!";
+    echo "<script type='text/javascript'>alert('$message');</script>";
+  }
+  else
+  {
+    header('Location:studentdb.php');
+  }
+}
+?>
+	</div>
+</body>
+</html>
---
+++ sms project/register.php
@@ -0,0 +1,105 @@
+<html>
+<head>
+<title>Sign Up Page</title>
+<link rel="stylesheet" href="style.css">
+<style type="text/css">
+body{
+	background-image: url('reg.jpg');
+	background-repeat: no-repeat;
+	background-size: 100%,100%;
+}
+#rgwrapper{
+	height: 650px;
+	border-radius : 10px;
+	background-color: #3498db;
+	border: 3px solid #f1f1f1;
+	width: 500px;
+	margin: 0 auto;
+}
+</style>
+</head>
+<body>
+	<div id="rgwrapper">
+	<center><br><br><h2>REGISTER FORM</h2></center>
+		<form action="<?php $_SERVER['PHP_SELF'];?>" method="post">
+			<div class="inner_container">
+				<label><b>Name</b></label>
+				<input type="text" placeholder="Enter Username" name="username" required>
+				<label><b>Address</b></label>
+				<input type="text" placeholder="Enter address" name="addr" required>
+				<label><b>Select Role</b></label><br><br>
+				<select name="sl"><option>Student</option><option>Teacher</option><option>Staff</option><option>principle</option></select>
+				<br><br><label><b>Password</b></label>
+				<input type="password" placeholder="Enter Password" name="password" required>
+				<label><b>Confirm Password</b></label>
+				<input type="password" placeholder="Enter Password" name="cpassword" required>
+				<button name="register" class="sign_up_btn" type="submit">Sign Up</button>
+				<a href="login.php"><button type="button" class="back_btn"><< Back to Login</button></a>
+			</div>
+		</form>
+	</div>
+		
+<?php
+if($_SERVER["REQUEST_METHOD"]=="POST")
+{
+	$sele=$_POST['sl'];
+  $name=$_POST["username"];
+  $pass=$_POST["password"];
+  $address=$_POST["addr"];
+  $pass0=$_POST["cpassword"];
+  $dbname="sms";
+   $servername = "localhost";
+   $username = "root";
+   $password = "";
+    $conn=new mysqli($servername, $username, $password);
+    $sql="CREATE DATABASE $dbname";
+    $conn->query($sql);
+    mysqli_select_db($conn,$dbname);
+    //$table1="CREATE TABLE student(name varchar(20),  primary key AUTO_INCREMENT,c_name VARCHAR(20),c_email VARCHAR(50),c_pass VARCHAR(20),c_address VARCHAR(50),c_mno BIGINT)";
+   // $conn->query($table1);
+    if($pass==$pass0)
+    {
+		if($sele=="student"){
+      $values="INSERT INTO student(name,address,password) VALUES('$name','$address','$pass')";
+        if($conn->query($values)===TRUE)
+              {
+                $message = "Registered Successfully!";
+                echo "<script type='text/javascript'>alert('$message');</script>";
+              }     
+      }
+	  else if($sele=="teacher")
+	  {
+      $values="INSERT INTO teacher(name,address,password) VALUES('$name','$address','$pass')";
+        if($conn->query($values)===TRUE)
+              {
+                $message = "Registered Successfully!";
+                echo "<script type='text/javascript'>alert('$message');</script>";
+              }     
+      }
+	  else if($sele=="staff")
+	  {
+      $values="INSERT INTO staff(name,address,password) VALUES('$name','$address','$pass')";
+        if($conn->query($values)===TRUE)
+              {
+                $message = "Registered Successfully!";
+                echo "<script type='text/javascript'>alert('$message');</script>";
+              }     
+      }
+	  else if($sele=="principle"){
+      $values="INSERT INTO principal(name,address,password) VALUES('$name','$address','$pass')";
+        if($conn->query($values)===TRUE)
+              {
+                $message = "Registered Successfully!";
+                echo "<script type='text/javascript'>alert('$message');</script>";
+              }     
+      }
+	}
+      else 
+      {
+        $passerr="Check password";
+        echo "<script type='text/javascript'>alert('$passerr');</script>";
+	}
+  }
+?>
+</body>
+</html>
